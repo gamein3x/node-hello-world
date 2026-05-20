3065e21f56a941f7a41a7216879f641192409e9c
Ivan Caldarella <gamein3x@gmail.com>
2026-05-20 17:44:30 +0200

    Codice base per stampare primo valore funziona

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello Boolean!');
+const inputArray = process.argv;
+
+let message = `Ciao ${inputArray[2]}`;
+
+if (inputArray.length > 3) {
+    for (let i = 3; i < inputArray.length; i++) {
+        const moreName = ` ${inputArray[i].toString()}`;
+        message += moreName;
+    }
+}
+
+console.log(message);
